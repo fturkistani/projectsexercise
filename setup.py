88a4ae71b00ae7f5a0b3c1fa7fc38730c2192889
Farah_Turkistani <ft8xx@virginia.edu>
2024-07-02 11:25:28 -0400

    all the files for project module

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='Test',
+    version='1.0.0',
+    url='https://github.com/fturkistani/projectsexercise',
+    author='Farah Turkistani',
+    author_email='ft8xx@virginia.edu',
+    description='Description of my package',
+    license='MIT',
+    packages=['packagedir']    
+)
